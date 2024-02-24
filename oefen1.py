cb7cfde18c9c804d34cc69663709e69d2cb3849b
QuintoniusB <Quintenbaris@gmail.com>
2024-02-24 12:37:14 -0500

    bs commit but just to keep track of progress

---
+++ oefen1.py
@@ -0,0 +1,22 @@
+## getting used to syntax, main language is c++
+y = {'quinten'}
+x = {f'ik heet {y}'}  
+## print(x)
+
+list1 = [1,2,3,4,5,6]
+list2 = ['hallo','hoi','doei',list1]
+## print(list2[0]) 
+
+dict1 = {'brand': 'mercedes', 'power': '800 hp'}
+#print(dict1['brand'])
+#print(dict1['power'])
+
+dict1['owners']= ['quinten','luca']
+#print(dict1)
+#print(dict1.keys())
+
+
+x = ('quinten', '20 jaaar oud', '195 cm lang', 'studeert informatica')
+print(x)
+
+#did if statements etc in course editor as exams
